c5db1c4c64cfdf18537e1b70f0b249e8e76479d7
Andre Malvadao <andre.paiva@unesp.br>
2022-10-24 17:19:36 -0300

    FIX: Menu

--- src/pages/Menu/menu.jsx
+++ src/pages/Menu/menu.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState, useContext } from 'react';
 import './menu.css';
 
 function Menu() {
-
+   return (
+      <>
+         
+      </>
+   );
 }
 export default Menu;
