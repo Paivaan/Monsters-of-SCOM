85352b897aac7214c7429e2eadd38f7217104263
GiulianoFaria <giuliano.de.faria@gmail.com>
2022-10-31 10:37:29 -0300

    FIT:mapa

--- src/pages/Menu/menu.jsx
+++ src/pages/Menu/menu.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState, useContext, li } from 'react';
 import { Link } from 'react-router-dom';
 import './menu.css';
+import city from '../../images/map_final.png';
+import teste from '../../images/dolar_png.png';
+
 
 function Menu() {
    return (
-      <section>
-         <h1><span>HOME</span></h1>
-      </section>
+      <>
+         <div>
+            <img src={city} alt="City" />
+
+            <div>
+               <img src={teste} alt="teste" />
+            </div>
+         </div>
+      </>
    );
 }
 export default Menu;
