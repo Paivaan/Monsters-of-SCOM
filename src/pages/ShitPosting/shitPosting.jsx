b45b89f3b79eae2c1be373fdd4ea237b50ea3a33
Fezao100 <goldfz@hotmail.com>
2022-11-01 18:22:55 -0300

    FIt: PARTE DO AUDIO

--- src/pages/ShitPosting/shitPosting.jsx
+++ src/pages/ShitPosting/shitPosting.jsx
@@ -8,7 +8,7 @@ import Piadocas from '../../components/piadocas/formulario.js'
 function ShitPosting() {
    return (
       <><div style={{ backgroundImage: `url(${fundo})` }}></div>
-     
+
       <section className= {styles.home_container}>
          <h1>Bem vindo a ala do <span>ShitPost!</span></h1>
          <p> Vem coringar com a gente, patrão</p>
@@ -22,26 +22,6 @@ function ShitPosting() {
          <h1>COLETOR DE PIADAS CORINGA-MASTER 2.0</h1>
          <Piadocas/>
       </div>
-      
-      <div class="top-bar">
-       <div class="container">
-           <div class="player-controls">
-               <span class="player-prev">Prev</span>
-               <span class="player-play">Play</span>
-               <span class="player-pause">Pause</span>
-               <span class="player-stop">Stop</span>
-               <span class="player-next">Next</span>
-           </div>
-           <div class="player"></div>
-           <div class="player-timeline">
-               <div class="player-timeline-control"></div>
-           </div>
-           <div class="player-display">
-               Playing: <span class="player-current-track"></span>
-           </div>
-       </div>
-   </div>
-      
       </>
    );
 }
