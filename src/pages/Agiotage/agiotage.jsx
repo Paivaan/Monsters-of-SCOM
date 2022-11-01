78c361ca2650faa79de5101b8cf2e8a9c9f27fe0
Andre Malvadao <andre.paiva@unesp.br>
2022-11-01 16:18:14 -0300

    FIT: Buscador de Inadimplentes

--- src/pages/Agiotage/agiotage.jsx
+++ src/pages/Agiotage/agiotage.jsx
@@ -1,29 +1,53 @@
 import React, { useEffect, useState, useContext } from 'react';
 import './agiotage.css';
 import { BiSearchAlt } from "react-icons/bi";
+import apiCEP from '../../services/axios';
 
 function Agiotage() {
-   return (
-      <div className='container_agiotage'>
-         <h1 className='title'>
-            Procure seu Inadimplente:
-         </h1>
-         <div className='containerInput'>
-            <input type="text" placeholder='Digite o CEP do malandro:' />
-            <button className='buttonCEP'>
-               Procurar
-               <BiSearchAlt size={20} />
-            </button>
-         </div>
-         <main className='main'>
-            <h2>CEP: 7987456123</h2>
-            <span>Rua: DASDASC</span>
-            <span>Complemento: afads</span>
-            <span>Bairro</span>
-            <span>Anápolis - GO</span>
-         </main>
 
-      </div>
-   );
+   const [input, setInput] = useState("")
+   const [cep, setCep] = useState({})
+
+   async function handleSearch() {
+      if (input === "") {
+         alert("Me ajude a te ajudar, preencha algum CEP!")
+         return;
+      }
+   
+   try {
+      const response = await apiCEP.get(`${input}/json`)
+      console.log('response :>> ', response.data);
+      setCep(response.data)
+      setInput("")
+   } catch {
+      alert("Eita mano, algum erro aconteceu!")
+      setInput("")
+   }
 }
+
+return (
+   <div className='container_agiotage'>
+      <h1 className='title'>
+         Procure seu Inadimplente:
+      </h1>
+      <div className='containerInput'>
+         <input type="text" placeholder='Digite o CEP do malandro:' value={input} onChange={(e) => setInput(e.target.value)} />
+         <button className='buttonCEP' onClick={handleSearch}>
+            Procurar
+            <BiSearchAlt size={20} />
+         </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (  
+         <main className='main'>
+         <h2>CEP: {cep.cep}</h2>
+         <span>Complemento: {cep.complemento}</span>
+         <span>{cep.bairro}</span>
+         <span>{cep.localidade} - {cep.uf}</span>
+      </main>
+         )}
+
+   </div>
+)};
+
 export default Agiotage;
---
+++ src/services/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const apiCEP = axios.create({
+   baseURL: "https://viacep.com.br/ws/"
+})
+export default apiCEP;
