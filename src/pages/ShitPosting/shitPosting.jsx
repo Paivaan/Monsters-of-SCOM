797ff77fb68b0d63b6189054d0e8f1c711e90382
GiulianoFaria <giuliano.de.faria@gmail.com>
2022-11-01 21:53:42 -0300

    FIT: forms

--- src/pages/ShitPosting/shitPosting.jsx
+++ src/pages/ShitPosting/shitPosting.jsx
@@ -7,7 +7,6 @@ import Piadocas from '../../components/piadocas/formulario.js'
 import axios from '../../services/axiosTabela';
 
 function ShitPosting() {
-<<<<<<< HEAD
 
     const [piadas, setPiadas] = useState([]);
 
@@ -87,25 +86,5 @@ function ShitPosting() {
 
         </>
     );
-=======
-   return (
-      <><div style={{ backgroundImage: `url(${fundo})` }}></div>
-
-      <section className= {styles.home_container}>
-         <h1>Bem vindo a ala do <span>ShitPost!</span></h1>
-         <b> Vem coringar com a gente, patrão</b>
-         <img src={coringar} alt="Coringuei" />
-         <b>Agora veja esse magnífico cachorro pancinha</b>
-         <img src={pancinha} alt="Pancinha" />
-         <b>Agora que você já se divertiu um pouco sua vez de colaborar com a firma!</b>
-         <b>Participe do nosso coletor de piadas e risadas!</b>
-      </section>
-      <div className= {styles.forms_container}>
-         <h1>COLETOR DE PIADAS CORINGA-MASTER 2.0</h1>
-         <Piadocas/>
-      </div>
-      </>
-   );
->>>>>>> 1c4aea86b9d76545e8e7aa96ac7aee938c79944e
 }
 export default ShitPosting;
