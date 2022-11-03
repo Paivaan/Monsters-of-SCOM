1af878f0f2b7b2dae9c55788f026803d4ff0da6e
Andre Malvadao <andre.paiva@unesp.br>
2022-11-03 19:05:28 -0300

    Estilização movida

--- src/pages/JDM/jdm.jsx
+++ src/pages/JDM/jdm.jsx
@@ -11,46 +11,46 @@ function JDM() {
 
    return (
       <div>
-         <div className="jdm">
+         <div>
             <div style={{ width: "900px", backgroundColor: "#000", margin: "0px auto 10px auto", boxShadow: "0px 0px 10px rgb(248, 5, 187)", padding: "10px" }}>
-               <div id="cabecalho">
+               <div>
                   <div>
                      <h1 style={{ paddingTop: "50px", color: "#f805bb" }}>BEST JDM CARS</h1>
                      <h2 style={{ color: "#fff" }}>ESCOLHA O SEU E ESCUTE O RONCO!</h2>
                   </div>
                </div>
 
-               <div className="container">
-                  <div className="box1">
-                     <div className="textinho">SKYLINE</div>
-                     <div className="imag1">
-                        <img alt='q' src={skyline} className="imgbox" />
+               <div>
+                  <div>
+                     <div>SKYLINE</div>
+                     <div>
+                        <img alt='q' src={skyline} />
 
                      </div>
                   </div>
-                  <div className="box2">
-                     <div className="textinho">RX-7</div>
-                     <div className="imag2">
-                        <img alt='2' src={rx7} className="imgbox" />
+                  <div>
+                     <div>RX-7</div>
+                     <div>
+                        <img alt='2' src={rx7} />
                      </div>
                   </div>
-                  <div className="box3">
-                     <div className="textinho">NISSAN SILVIA</div>
-                     <div className="imag3">
-                        <img alt='3' src={silvia} className="imgbox" />
+                  <div>
+                     <div>NISSAN SILVIA</div>
+                     <div>
+                        <img alt='3' src={silvia} />
                      </div>
                   </div>
-                  <div className="box4">
-                     <div className="textinho">SUBARU</div>
-                     <div className="imag4">
-                        <img alt='4' src={subarus} className="imgbox" />
+                  <div>
+                     <div>SUBARU</div>
+                     <div>
+                        <img alt='4' src={subarus} />
                      </div>
                   </div>
                   
                </div>
 
 
-               <h2 id="vai-tremer">BRUP BRUP BRUP</h2>
+               <h2>BRUP BRUP BRUP</h2>
 
             </div>
 
